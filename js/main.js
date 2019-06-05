827b3bc24c04aeeb96de83c659917decb7806148
Adam Plouff <adamplouff@gmail.com>
2019-06-05 14:18:56 -0400

    Visual updates and tutorials

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ $(window).load(function() {
 $(document).ready(function(){
 
 
-     
+
     //animated header class
-    $(window).scroll(function() {    
+    $(window).scroll(function() {
     var scroll = $(window).scrollTop();
      //console.log(scroll);
     if (scroll > 200) {
@@ -33,12 +33,27 @@ $(document).ready(function(){
     $('.image-popup').magnificPopup({type:'image'});
 
 
+    var $videoSrc;
+    $('.video-btn').click(function() {
+        $videoSrc = $(this).data( "src" );
+    });
+    console.log($videoSrc);
+
+
+
+    // when the modal is opened autoplay it
+    $('#myModal').on('shown.bs.modal', function (e) {
+
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" );
+    })
+
+
+
+    // stop playing the youtube video when I close the modal
+    $('#myModal').on('hide.bs.modal', function (e) {
+        // a poor man's stop video
+        $("#video").attr('src',$videoSrc);
+    })
 
 });
-
-
-
-
-
-
-
